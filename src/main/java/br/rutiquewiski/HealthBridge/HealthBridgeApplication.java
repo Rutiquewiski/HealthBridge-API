2db0e82947dd56646e731924ace45e666a1bc7f2
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2023-11-07 20:37:38 -0300

    Api start comment

--- src/main/java/br/rutiquewiski/HealthBridge/HealthBridgeApplication.java
+++ src/main/java/br/rutiquewiski/HealthBridge/HealthBridgeApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HealthBridgeApplication {
 
+
+	//Start API
 	public static void main(String[] args) {
 		SpringApplication.run(HealthBridgeApplication.class, args);
 	}
