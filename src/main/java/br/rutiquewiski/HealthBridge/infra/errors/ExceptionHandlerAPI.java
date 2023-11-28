af5355829ad66746f8b8859423e6026e9e9f0dcc
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2023-11-28 15:08:24 -0300

    Duplicate exception handler creation

--- src/main/java/br/rutiquewiski/HealthBridge/infra/errors/ExceptionHandlerAPI.java
+++ src/main/java/br/rutiquewiski/HealthBridge/infra/errors/ExceptionHandlerAPI.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @RestControllerAdvice
 public class ExceptionHandlerAPI {
 
@@ -29,5 +31,11 @@ public class ExceptionHandlerAPI {
     public ResponseEntity<?> handleValidationException(ValidationException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
         //This method handles the exception that we created to manage validations
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<?> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+        //This method handles the exception that happens when the API receives and DTO with a value on a field that does not accept duplicates
     }
 }
