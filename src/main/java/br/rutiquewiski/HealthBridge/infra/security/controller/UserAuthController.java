acadc9372dbbb87524c160120ed21b6a8ce4a8b4
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2023-12-10 18:55:40 -0300

    User auth controller creation

---
+++ src/main/java/br/rutiquewiski/HealthBridge/infra/security/controller/UserAuthController.java
@@ -0,0 +1,9 @@
+package br.rutiquewiski.HealthBridge.infra.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserAuthController {
+}
