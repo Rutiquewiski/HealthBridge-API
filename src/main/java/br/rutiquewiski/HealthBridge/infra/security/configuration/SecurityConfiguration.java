dbec2c19899e5fbf2e34e630a7c8b649902d88ac
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2024-01-10 10:25:01 -0300

    api key reset

--- src/main/java/br/rutiquewiski/HealthBridge/infra/security/configuration/SecurityConfiguration.java
+++ src/main/java/br/rutiquewiski/HealthBridge/infra/security/configuration/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package br.rutiquewiski.HealthBridge.infra.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -54,4 +56,11 @@ public class SecurityConfiguration {
         }
 
     }
+
+    @Configuration
+    @EnableWebSecurity
+    @Order(1)
+    public static class ApiKeySecurityConfig {}
+
+
 }
