8bcdc5395dc7b6b89b0ffc720d62dc46175d12ba
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2024-02-17 13:51:13 -0300

    Auth temporary fix

--- src/main/java/br/rutiquewiski/HealthBridge/infra/security/configuration/SecurityConfiguration.java
+++ src/main/java/br/rutiquewiski/HealthBridge/infra/security/configuration/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration {
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authorizeHttpRequests(auth -> {
                                 auth.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
-                                auth.requestMatchers( AntPathRequestMatcher.antMatcher("/api/key/**") ).authenticated();
+                                auth.requestMatchers( AntPathRequestMatcher.antMatcher("/api/**") ).authenticated(); //Change to /api/key/**
                             }
                     )
                     .addFilterBefore(keySecurityFilter, UsernamePasswordAuthenticationFilter.class)
@@ -53,39 +53,39 @@ public class SecurityConfiguration {
 
     }
 
-    @EnableWebSecurity
-    @Configuration
-    @Order(2)  //This annotation sets this configuration to be the second one in the auth order
-    public static class StandardSecurityConfiguration {
-
-        //Regular token auth
-
-        @Autowired
-        private TokenSecurityFilter tokenSecurityFilter;
-
-        @Bean
-        public SecurityFilterChain tokenSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-            return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .authorizeHttpRequests(auth -> {
-                        auth.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
-                        auth.anyRequest().authenticated();
-                    })
-                    .addFilterBefore(tokenSecurityFilter, UsernamePasswordAuthenticationFilter.class)
-                    .build();
-        }
-
-        @Bean
-        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-            return  authenticationConfiguration.getAuthenticationManager();
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
-
-    }
+//    @EnableWebSecurity
+//    @Configuration
+//    @Order(2)  //This annotation sets this configuration to be the second one in the auth order
+//    public static class StandardSecurityConfiguration {
+//
+//        //Regular token auth
+//
+//        @Autowired
+//        private TokenSecurityFilter tokenSecurityFilter;
+//
+//        @Bean
+//        public SecurityFilterChain tokenSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//            return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+//                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                    .authorizeHttpRequests(auth -> {
+//                        auth.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
+//                        auth.anyRequest().authenticated();
+//                    })
+//                    .addFilterBefore(tokenSecurityFilter, UsernamePasswordAuthenticationFilter.class)
+//                    .build();
+//        }
+//
+//        @Bean
+//        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//            return  authenticationConfiguration.getAuthenticationManager();
+//        }
+//
+//        @Bean
+//        public PasswordEncoder passwordEncoder() {
+//            return new BCryptPasswordEncoder();
+//        }
+//
+//    }
 
 }
 
