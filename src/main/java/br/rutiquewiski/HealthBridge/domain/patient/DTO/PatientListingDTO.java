6032b6bafd81bfa125c6c1195f7eb124e6d9549e
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2024-03-19 12:47:04 -0300

    Issue Fix: Dentist and Doctor wihout ID #24

--- src/main/java/br/rutiquewiski/HealthBridge/domain/patient/DTO/PatientListingDTO.java
+++ src/main/java/br/rutiquewiski/HealthBridge/domain/patient/DTO/PatientListingDTO.java
@@ -3,12 +3,13 @@ package br.rutiquewiski.HealthBridge.domain.patient.DTO;
 import br.rutiquewiski.HealthBridge.domain.patient.Patient;
 
 public record PatientListingDTO(
+        int id,
         String name,
         String email,
         String phone,
         String document
 ) {
     public PatientListingDTO(Patient patient) {
-        this(patient.getName(), patient.getEmail(), patient.getPhone(), patient.getDocument());
+        this(patient.getId(), patient.getName(), patient.getEmail(), patient.getPhone(), patient.getDocument());
     }
 }
--- src/main/java/br/rutiquewiski/HealthBridge/domain/professional/dentist/DTO/DentistListingDTO.java
+++ src/main/java/br/rutiquewiski/HealthBridge/domain/professional/dentist/DTO/DentistListingDTO.java
@@ -6,6 +6,7 @@ import br.rutiquewiski.HealthBridge.domain.professional.dentist.Dentist;
 import java.util.List;
 
 public record DentistListingDTO(
+        int id,
         String name,
         String email,
         String phone,
@@ -14,6 +15,6 @@ public record DentistListingDTO(
 ) {
 
     public DentistListingDTO(Dentist dentist) {
-        this(dentist.getName(), dentist.getEmail(), dentist.getPhone(), dentist.getDocument(), dentist.getSpecialties());
+        this(dentist.getId(), dentist.getName(), dentist.getEmail(), dentist.getPhone(), dentist.getDocument(), dentist.getSpecialties());
     }
 }
