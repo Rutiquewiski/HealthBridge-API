d318f161af869118d95367086089cff1b748aa7d
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2024-03-22 11:11:24 -0300

    Appointments listing endpoint creation

--- src/main/java/br/rutiquewiski/HealthBridge/controllers/DentistAppointmentController.java
+++ src/main/java/br/rutiquewiski/HealthBridge/controllers/DentistAppointmentController.java
@@ -3,10 +3,14 @@ package br.rutiquewiski.HealthBridge.controllers;
 import br.rutiquewiski.HealthBridge.domain.appointment.AppointmentManager;
 import br.rutiquewiski.HealthBridge.domain.appointment.dentist.DTO.CreateDentistAppointmentDTO;
 import br.rutiquewiski.HealthBridge.domain.appointment.dentist.DTO.CreatedDentistAppointmentDTO;
+import br.rutiquewiski.HealthBridge.domain.appointment.dentist.DTO.DentistAppointmentListingDTO;
 import br.rutiquewiski.HealthBridge.infra.errors.ValidationException;
+import br.rutiquewiski.HealthBridge.repositories.AppointmentDentistRepository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +21,9 @@ public class DentistAppointmentController {
 
     @Autowired
     private AppointmentManager appointmentManager;
+
+    @Autowired
+    private AppointmentDentistRepository appointmentDentistRepository;
 
     @PostMapping
     public ResponseEntity<CreatedDentistAppointmentDTO> createDentistAppointment(@RequestBody @Valid CreateDentistAppointmentDTO createDentistAppointmentDTO) throws ValidationException {
@@ -31,5 +38,22 @@ public class DentistAppointmentController {
         appointmentManager.cancelDentistAppointment(id);
 
         return ResponseEntity.ok("Appointment canceled");
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllAppointments(Pageable pageable) {
+
+        Page<DentistAppointmentListingDTO> appointments;
+
+        appointments = appointmentDentistRepository.findAll(pageable).map(DentistAppointmentListingDTO::new);
+
+        if (appointments.isEmpty()) {
+
+            return ResponseEntity.ok("Empty List");
+
+        } else {
+
+            return ResponseEntity.ok(appointments);
+        }
     }
 }
--- src/main/java/br/rutiquewiski/HealthBridge/repositories/AppointmentDoctorRepository.java
+++ src/main/java/br/rutiquewiski/HealthBridge/repositories/AppointmentDoctorRepository.java
@@ -1,6 +1,8 @@
 package br.rutiquewiski.HealthBridge.repositories;
 
 import br.rutiquewiski.HealthBridge.domain.appointment.doctor.Appointment_Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,6 @@ public interface AppointmentDoctorRepository extends JpaRepository<Appointment_D
     boolean existsByDoctorIdAndDate(Integer doctorId, LocalDateTime date);
 
     boolean existsByPatientIdAndDateBetween(Integer patientId, LocalDateTime first, LocalDateTime last);
+
+    Page<Appointment_Doctor> findAll(Pageable pageable);
 }
